c7ec2345c4db053206886d361a195e18b79ad4c3
zeynelok <zeynel_2406@hotmail.com>
2022-05-30 23:42:49 +0300

    ProductsController updated

--- NLayer.WebApi/Controllers/ProductsController.cs
+++ NLayer.WebApi/Controllers/ProductsController.cs
@@ -11,19 +11,17 @@ namespace NLayer.WebApi.Controllers
     public class ProductsController : CustomBaseController
     {
         private readonly IMapper _mapper;
-        private readonly IService<Product> _service;
         private readonly IProductService _productService;
-        public ProductsController(IMapper mapper, IService<Product> service,IProductService productService)
+        public ProductsController(IMapper mapper,IProductService productService)
         {
-            _mapper = mapper;
-            _service = service;
+            _mapper = mapper;     
             _productService = productService;
         }
 
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            var products = await _service.GetAllAsync();
+            var products = await _productService.GetAllAsync();
             var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
 
             return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
@@ -33,7 +31,7 @@ namespace NLayer.WebApi.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var product = await _service.GetByIdAsync(id);
+            var product = await _productService.GetByIdAsync(id);
             var productDto = _mapper.Map<ProductDto>(product);
             return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productDto));
         }
@@ -41,7 +39,7 @@ namespace NLayer.WebApi.Controllers
         [HttpPost]
         public async Task<IActionResult> Save(ProductDto productDto)
         {
-            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var product = await _productService.AddAsync(_mapper.Map<Product>(productDto));
             var newProductDto = _mapper.Map<ProductDto>(product);
             return CreateActionResult(CustomResponseDto<ProductDto>.Success(201, newProductDto));
         }
@@ -50,15 +48,15 @@ namespace NLayer.WebApi.Controllers
         [HttpPut]
         public async Task<IActionResult> Update(ProductUpdateDto productUpdateDto)
         {
-            await _service.UpdateAsync(_mapper.Map<Product>(productUpdateDto));
+            await _productService.UpdateAsync(_mapper.Map<Product>(productUpdateDto));
             return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Remove(int id)
         {
-            var product = await _service.GetByIdAsync(id);
-            await _service.RemoveAsync(product);
+            var product = await _productService.GetByIdAsync(id);
+            await _productService.RemoveAsync(product);
             return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
         }
 
