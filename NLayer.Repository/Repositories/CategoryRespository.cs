a247c664b4807e30d2b73552bb106b1077599dd4
zeynelok <zeynel_2406@hotmail.com>
2022-05-30 22:46:23 +0300

    category repository added

---
+++ NLayer.Repository/Repositories/CategoryRespository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Repositories
+{
+    public class CategoryRespository : GenericRepository<Category>, ICategoryRespository
+    {
+        public CategoryRespository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetSingleCategoryByIdWithProductsAsync(int categoryId)
+        {
+            return await _context.Categories.Include(c => c.Products).Where(c => c.Id == categoryId).SingleOrDefaultAsync();
+        }
+    }
+}
