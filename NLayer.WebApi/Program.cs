7dbe02b7e7237d8f43d7847061a2bfc9d10aaef3
zeynelok <zeynel_2406@hotmail.com>
2022-08-04 00:19:20 +0300

    Caching process started

--- NLayer.WebApi/Program.cs
+++ NLayer.WebApi/Program.cs
@@ -33,6 +33,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
 
@@ -49,6 +50,7 @@ builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
+
 
 var app = builder.Build();
 
