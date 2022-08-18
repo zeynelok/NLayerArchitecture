8de04c55a87b4a59ba9f5af4aaf1e93c76e677a1
zeynelok <zeynel_2406@hotmail.com>
2022-08-18 21:40:20 +0300

    caching added

--- NLayer.Service/Services/ProductServiceWithNoCaching.cs
+++ NLayer.Service/Services/ProductServiceWithNoCaching.cs
@@ -12,11 +12,11 @@ using System.Threading.Tasks;
 
 namespace NLayer.Service.Services
 {
-    public class ProductService : Service<Product>,IProductService
+    public class ProductServiceWithNoCaching : Service<Product>,IProductService
     {
         private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
-        public ProductService(IGenericRepository<Product> genericRepository, IUnitOfWork unitOfWork, IProductRepository productRepository,IMapper mapper) : base(genericRepository, unitOfWork)
+        public ProductServiceWithNoCaching(IGenericRepository<Product> genericRepository, IUnitOfWork unitOfWork, IProductRepository productRepository,IMapper mapper) : base(genericRepository, unitOfWork)
         {
 
             _productRepository = productRepository;
--- NLayer.WebApi/Modules/RepoServiceModule.cs
+++ NLayer.WebApi/Modules/RepoServiceModule.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using NLayer.Caching;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -33,6 +34,8 @@ namespace NLayer.WebApi.Modules
                .Where(x => x.Name.EndsWith("Service"))
                .AsImplementedInterfaces()
                .InstancePerLifetimeScope();
+
+            builder.RegisterType<ProductServiceWithCaching>().As<IProductService>();
 
             base.Load(builder);
         }
